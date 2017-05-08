3548ac83243e2bf6aaffaa016ab5c9336b70a584
zhangjianxin1 <zhang-rd-fe@s139.org>
2017-05-08 11:15:09 +0800

    更新概要：第一次初始化项目

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# ng4-xterm
+# NG4-Xterm
+IMPORT XTERMJS [![xterm.js build status](https://api.travis-ci.org/sourcelair/xterm.js.svg)]
+
+## NG4-Xterm 功能介绍
+
+- 主要是要实现在线的容器bash命令操作。   
+- 文件在线导出等功能。
+- 本工具中其中一部分用到了 xtermjs的工具类，感谢作者开源。
+- Xterm is not a terminal application that you can download and use on your computer
+- Xterm is not `bash`. Xterm.js can be connected to processes like `bash` and let you interact with them (provide input, receive output)
 
 ```shell
 
-npm install -y ng4-xterm --save
+npm install  ng4-xterm --save -dd
 ```
+
+
+@ Owner <https://firsh.me>
+
+@ Github <https://github.com/zmatsh>
+
+```shell
+目前正在开发阶段～
+```
+
---
+++ libs/main.js
@@ -0,0 +1,89 @@
+(function () {
+    var d = document,
+        w = window,
+        p = parseInt,
+        dd = d.documentElement,
+        db = d.body,
+        dc = d.compatMode == 'CSS1Compat',
+        dx = dc ? dd: db,
+        ec = encodeURIComponent;
+
+
+    w.Xterm = {
+        msgObj:d.getElementById("message"),
+        screenheight:w.innerHeight ? w.innerHeight : dx.clientHeight,
+        containerId:null,
+        socket:null,
+        //让浏览器滚动条保持在最低部
+        scrollToBottom:function(){
+            w.scrollTo(0, this.msgObj.clientHeight);
+        },
+        //退出，本例只是一个简单的刷新
+        logout:function(){
+            this.socket.disconnect();
+            location.reload();
+        },
+        //提交聊天消息内容
+        submit:function(){
+            var content = d.getElementById("content").value;
+            if(content != ''){
+                var obj = {
+                    containerid: this.containerId,
+                    content: content
+                };
+                this.socket.emit('message', obj);
+                d.getElementById("content_bash").value = '';
+            }
+            return false;
+        },
+        genUid:function(){
+            return new Date().getTime()+""+Math.floor(Math.random()*899+100);
+        },
+
+        init:function(containerId){
+            /*
+             客户端根据时间和随机数生成uid,这样使得聊天室用户名称可以重复。
+             实际项目中，如果是需要用户登录，那么直接采用用户的uid来做标识就可以
+             */
+            this.containerId = containerId;
+
+            //连接websocket后端服务器
+            this.socket = io.connect('ws://cgroup.itd:3000');
+
+            //告诉服务器端有用户登录
+            this.socket.emit('create', {containerid:this.containerId});
+
+            //监听数据
+            this.socket.on('start', function(o){
+
+            });
+
+
+            //监听消息发送
+            this.socket.on('message', function(obj){
+                var isme = (obj.containerId == Xterm.containerId) ? true : false;
+                var contentDiv = '<div>'+obj.content+'</div>';
+                var containerDiv = '<span>'+obj.containerId+'</span>';
+
+                var section = d.createElement('section');
+                if(isme){
+                    section.className = 'containerID';
+                    section.innerHTML = contentDiv + containerDiv;
+                } else {
+                    section.className = 'service';
+                    section.innerHTML = containerDiv + contentDiv;
+                }
+                Xterm.msgObj.appendChild(section);
+                Xterm.scrollToBottom();
+            });
+
+        }
+    };
+    //通过“回车”提交信息
+    d.getElementById("content_bash").onkeydown = function(e) {
+        e = e || event;
+        if (e.keyCode === 13) {
+            Xterm.submit();
+        }
+    };
+})();
